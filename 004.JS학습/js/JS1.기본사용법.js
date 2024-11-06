1c4f011f6b961259e421b3d2030e413b31562789
ToffeeSta <Wjdgnsrh@gmail.com>
2024-11-06 17:11:44 +0900

    ㅇ

--- 004.JS학습/js/JS1.기본사용법.js
+++ 004.JS학습/js/JS1.기본사용법.js
@@ -29,11 +29,80 @@ function 김비서나와라() {
     )
   );
 
-  // 3-1. 배경이미지 넣기
-  document
+  // 변수에 대상 할당하기
+  var pbox1 = document
     .getElementsByClassName("pbox")
-    .item(0).style.background =
+    .item(0);
+  // 변수는 특정메모리 공간이다!
+  // 선언과 동시에 할당하여 값을 저장함
+  // 변수는 여기를 "봐!" var 로 선언
+  // var 변수명 = 값
+
+  // [변수를 사용하는 이유?]
+  // - 변수는 재사용
+  // - 변수는 저장 메모리공간 주소
+  // - 변수는 따옴표안쓴 문자
+  // - 변수는 띄어쓰기 없음
+  // - 변수는 호출시 값 출력
+  // - 변수는 재할당시 다른값 덮어씀
+  // - 변수는 한번만 선언 var(let/const)
+  // - var는 variable(변수)라는 단어에서 나온말!
+
+  ////// 변경내용 ////////////////////
+  // 3-1. 배경이미지 넣기
+  pbox1.style.background =
     "url(./images/kimbs.jpg) no-repeat top/cover";
   // .pbox중 첫번째 (0번)를 선택함!
   // item(순번)
+
+  // 3-2. 글자색 변경하기
+  pbox1.style.color = "white";
+
+  // 3-3. 글자 크기 변경하기
+  pbox1.style.fontSize = "40px";
+
+  // 3-4. 글자 그림자 적용하기
+  pbox1.style.textShadow =
+    "2px 2px 2px #000"; // (x, y, blur, color)
+
+  // 3-5. 트랜지션
+  // 백틱은 줄바꿈 자유로움
+  pbox1.style.transition = `
+    2s,
+    top 4s 2s,
+    left 2s 2s,
+    rotate 3s 3s,
+    translate 1s 2s,
+    scale 2s 6s
+  `;
+
+  // 3-6. 크기 변경
+  pbox1.style.width = "600px";
+  pbox1.style.height = "600px";
+
+  // 3-7. 줄간격
+  pbox1.style.lineHeight = "100px";
+
+  // 3-8. 원 만들기
+  pbox1.style.borderRadius = "50%";
+
+  // 3-9 중앙 이동을 위한 top,left 값 변경
+  pbox1.style.top = "50%";
+  pbox1.style.left = "50%";
+
+  // 3-10 트랜스폼 변경 : 중앙이동, 360도 회전, 스케일 변경
+  pbox1.style.translate = " -50% -50% ";
+  pbox1.style.rotate = "360deg ";
+  pbox1.style.scale = "1.3 ";
+  // pbox1.style.transform = "translate(-50%, -50%) rotate(360deg) scale(1.3)";
+  // 만약 속성명을 잘못쓰면 브라우저가 출력에서 제외한다!
+  // 예전 브라우저는 에러 발생했지만, 요즘은 에러 안남!
+
+  // 전체 배경색 바꾸기
+  // 대상: body
+  // JS에서 body는 특별히 취급 document.body로 선택
+  var body = document.body;
+  body.style.backgroundColor =
+    "lightgreen";
+  body.style.transition = " 3s 5s ";
 } //////////// 김비서나와라 함수 //////////
