ce7c6589dba998e2b9620263d812264274e0e7e1
ToffeeSta <Wjdgnsrh@gmail.com>
2024-11-29 11:03:39 +0900

    ㅇㅇ

--- 004.JS학습/js/JS3-4.for문연습1.js
+++ 004.JS학습/js/JS3-4.for문연습1.js
@@ -17,7 +17,7 @@
     [2] 미니언즈 개수 표시하기
     1. 미니언즈를 넣을때 실시간 반영 개수를 표시한다.
     2. 이때 미니언즈 이미지가 3개씩 있으므로
-    3의 배수로 표시해 준다!
+        3의 배수로 표시해 준다!
 
     ((대상선정))
     (1) 이벤트대상 : .mini (click이벤트)
@@ -36,93 +36,103 @@
 
 ***********************************************/
 
-// 1. 대상선정 ////////////////////////////////////
+// 1. 대상선정 //////////////////////
 // 1-1. 이벤트 대상 : .mini
 let mini =
   document.querySelectorAll(".mini");
-
-// 1-2. 변경 대상 :.mini-space
+// 1-2. 변경 대상 : .mini-space
 let miniSpace = document.querySelector(
   ".mini-space"
 );
+// 1-3. 개수출력요소 : .stxt span
+let stxt = document.querySelector('.stxt span');
 
-// 1-3. 개수 출력요소 .stxt span
-let stxt = document.querySelector(
-  ".stxt span"
-);
+// 1-4. 리셋버튼 : 
+let rbtn = document.querySelector('.rbtn');
 
-// 1-4. 리셋 버튼 .rbtn
-let rbtn =
-  document.querySelector(".rbtn");
+// console.log("선택요소:",
+    // mini,
+    // mini.length+"개",
+    // miniSpace,
+    // stxt,
+    // rbtn);
 
-// console.log("선택요소:", mini, mini.length + "개", miniSpace, stxt, rbtn);
+// 2. 이벤트 속성 셋팅하기 ///////////////
+// 이벤트 대상에게 click이벤트와 함수를 연결한다!
 
-// 2. 이벤트 속성 셋팅하기 ///////////////////////////
-// 이벤트 대상에게 click 이벤트와 함수를 연결한다
+// mini.item(0).onclick = insertMini;
 
-// 변수에 담긴 html컬렉션의 개수는? 변수.length
-// 개수만큼 for문을 돌아서 클릭 이벤트를 세팅한다!
+// mini에 담긴 html컬렉션의 개수는? 변수.length
+// 개수만큼 for문을 돌아서 클릭 이벤트 셋팅을 한다!
+// for(시;한;증){코드}
 
-// mini.length miniCnt(count)변수에 담기
-const cnt = mini.length;
+// 전체 mini개수
+const miniCnt = mini.length;
 
-for (let i = 0; i < cnt; i++) {
-  // mini.item(0).onclick = insertMini;
+for (let i = 0; i < miniCnt; i++) {
   mini[i].onclick = insertMini;
-} // for문 //////////////////
+} /// for ///
+
 
 // 3. 함수 만들기 ///////////////////////////
-// 미니언즈 이미지 개수를 저장할 변술를 함수 구역 바깥에서 만들고 전역변수로 선언
-let miniCnt = 0;
+// 미니언즈 이미지 개수를 저장할 변수를
+// 함수구역 바깥에 만들고 전역변수로 설정하여
+// 함수에서 변경시 그 값이 유지되도록 설정한다!
+let miniCount = 0;
+
 function insertMini() {
   // this -> 호출한 요소 자신!
-  // 1. 호출한 요소의 개수 속성 읽어오기 : data-num
-  let miniNum =
-    this.getAttribute("data-num");
 
-  // 속성값 읽어오기 2가지
-  // 1) 속성요소.속성명
-  // 2) 속성요소.getAttribute(속성명)
+  // 1. 호출한 요소의 개수속성 읽어오기 : data-num
+  let num = this.getAttribute('data-num');
+  
+  // [속성값 읽어오기 2가지]
+  // 1) 선택요소.속성명
+  // 2) 선택요소.getAttribute(속성명)
 
-  // 속성값 셋팅법 2가지
+  // [속성값 셋팅법 2가지]
   // 1) 선택요소.속성명 = 값
   // 예) document.querySelector('img').src = 경로
-  // 2) 선택요소.setAttribute(속성명, '값')
-  // 예) document.querySelector('img').setAttribute("src", 경로)
+  // 2) 선택요소.setAttribute(속성명,값)
+  // 예) document.querySelector('img')
+  //    .setAttribute("src",경로)
 
-  // 2. 함수 호출 확인
-  // console.log(
-  //   "미니언즈 넣어봐바!",
-  //   miniNum
-  // );
 
-  // 3. 변경대상(miniSpace)요소에 이미지 넣기
-  // += 대입 연산자 : 기존값에 새로운 값을 더함!
-  for (let i = 0; i < miniNum; i++) {
-    miniSpace.innerHTML += `<img
-                src="./images/Minions.png"
-                alt="미니언즈"/>`;
+  // 2. 함수호출확인
+  console.log("미니언즈 넣어바바~~!",num);
 
-    // miniCnt에 미니언즈 개수 증가하기
-    miniCnt++;
-  } ///// for문 //////////////////
+  // 3. 변경대상요소(miniSpace)에 이미지 넣기 
+  // += 대입연산자 : 기존값에 새로운값을 더함!
+  for(let i=0; i<num; i++) {
+      miniSpace.innerHTML += `
+        <img src="./images/Minions.png" alt="미니언즈">
+      `;
+      // 미니언즈 개수 증가하기
+      miniCount++;
 
-  // 4. 미니언즈 개수 표시하기 //////////
-  // 대상 : .stxt span -> stxt
-  stxt.innerText = miniCnt * 3;
-} //////////// insertMini() ////////////
+  } //// for ////  
 
-// 4. 리셋 버튼 이벤트 구현///////
-// 이벤트 버튼: .rbtn -> rbtn 변수
-rbtn.onclick = function () {
-  // console.log("reset");
+  // 4. 미니언즈 개수 표시하기 ///////
+  // 대상: .stxt span -> stxt변수
+  stxt.innerText = miniCount*3;
+  // 미니언즈 이미지에 3개씩 있으므로 곱하기3
 
-  // 2. 리셋버튼을 클릭하면 파란박스의
-  // 미니언즈 이미지 모두가 삭제 된다.
-  miniSpace.innerHTML = "";
+} ////////// insertMini 함수 ////////////////
 
-  // 3. 이때 미니언즈 개수도 0으로 초기화한다.
-  miniCnt = 0;
-  stxt.innerText = miniCnt;
+// 4. 리셋버튼 클릭시 초기화기능 ////////////////
+// 이벤트 대상: .rbtn -> rbtn변수
+rbtn.onclick = function(){
+    // 1. 함수호출확인
+    console.log("다~지워~~!");
 
-}; ////// click 이벤트 함수 ////////////////
+    // 2. 리셋버튼을 클릭하면 파란박스의 
+    // 미니언즈 이미지 모두가 삭제 된다.
+    miniSpace.innerHTML = "";
+
+    // 3. 이때 미니언즈 개수도 0으로 초기화한다.
+    stxt.innerText = '0';
+
+    // 4. 미니언즈 개수 전역변수도 초기화!
+    miniCount = 0;
+
+}; ////// click 이벤트함수 /////////////
--- 004.JS학습/js/JS4-1.배열과객체.js
+++ 004.JS학습/js/JS4-1.배열과객체.js
@@ -30,8 +30,7 @@
 **********************************************/
 
 // 1. 배열 셋팅하고 출력하기 ////////////////
-
-// 1-1. new 키워드로 배열선언 및 할당하기 ///////////////////////////
+// 1-1. new 키워드로 배열선언 및 할당하기 /////////////////
 // 그런데 배열은 굳이 new키워드로 선언할 필요는 없다!
 // 배열 리터럴로 생성할 수 있다!
 const arr1 = new Array(
@@ -46,68 +45,73 @@ const arr1 = new Array(
   ],
   "김태리",
   function () {
-    // this는 누구? 호출한 요소 자신
-    // alert("김태리 멋짐!!!");
-    // 1. 호출한 요소 박스에 김태리 사진 이미지 넣기
+    // this는 누구? 호출한 요소자신!!!
+    // alert("김태리 멋찜!!!");
+
+    // 1.호출한요소 박스에 김태리 사진 이미지를 넣고(+=대입연산자로 기존 데이터 살림!)
     this.innerHTML += `
-      <img src="https://cdn.iconsumer.or.kr/news/photo/202210/25071_29865_256.jpg" 
-        alt="김태리"   
-        style = "
+      <img src="https://cdn.mhnse.com/news/photo/202410/333921_379284_5123.jpg" alt="김태리이미지"
+        style="
           position: absolute;
           top: 0;
           left: 0;
-          width: 250px; 
-          height: 300px; 
-          border-radius: 10%;
-          display: none;
+          width: 150px;
+          height: 200px;
+          border-radius: 20px;
+          border: 4px ridge hotpink;    
+          display: none; 
           transition: .4s ease-out;
           translate: -50% -50%;
           z-index: 100;
           pointer-events: none;
         "
         id="kim"
-      />`;
-    // 위에 CSS 코드 체크
+      >
+    `;
+
+    // [위의 CSS 코드 체크!!!]
     // pointer-events: none -> 실제 이벤트는 부모박스인데
-    // 자식 요소인 이미지가 매번 가림으로 인해 이벤트가
-    // 순간 없어졌다가 다시 걸렸다가 함으로 떨림 증상 발생!
+    // 자식요소인 이미지가 매번 가림으로 인해 이벤트가
+    // 순간 없어졌가 다시 걸렸다가 함으로 떨림증상발생!!!
     // 이 설정으로 본 이미지는 이벤트를 없애고 아래쪽 요소가
-    // 그대로 이벤트를 발생하게 됨! 따라다니는 기능에서
-    // 매우 중요한 설정임!
-    // 앱솔루트의 부모자격은  this에게 준다
+    // 그대로 이벤트를 발생하게됨! 따라다니는 기능에서
+    // 매우 중요한 설정임!!!
+
+    // 앱솔루트의 부모자격을 this에게 준다!
     this.style.position = "relative";
 
-    // 위에서 생성된 id = "kim" 요소를 변수에 할당한다
+    // 위에서 생성된 id="kim" 요소를 변수에 할당함!
     const kim =
       document.querySelector("#kim");
 
-    // 2. 마우스 오버 시, 나타남
+    // 2.마우스 오버시 나타나고
     this.onmouseenter = () => {
-      document.querySelector;
       kim.style.display = "block";
     };
-    // 3. 마우스 아웃시, 사라짐
+    // 3.마우스 아웃시 사라지고
     this.onmouseleave = () => {
-      document.querySelector;
       kim.style.display = "none";
     };
-    // 4. 마우스 움직이면 따라다니게 하기
-    // mousemove 이벤트 : 마우스 포인터가 대상 요소 위에서 움직일 때 계속 발생
+    // 4.마우스 움직이면 따라다니게 해요!
+    // mousemove 이벤트 :
+    // 마우스 포인터가 대상요소 위에서 움직일때 계속발생
     this.onmousemove = (헐) => {
-      // 헐 변수는 이벤트 전달 변수임!
-      // 어떤 함수도 전달 값이 없는데 변수 하나를 쓰면
+      // "헐" 변수는 이벤트 전달변수임!
+      // 어떤 함수도 전달값이 없는데 변수하나를 쓰면
       // 곧 그것이 이벤트 전달변수가 됨!
       // 그 요소에서 발생하는 이벤트를 객체로 가지고 있음
-      // 대체해서 event라고 직접 전체 이벤트 객체를 쓸 수 있음
+      // 대체해서 event 라고 직접 전체 이벤트 객체를 쓸 수 있음!
 
-      // 이벤트 객체하위 pageX, pageY는 최상단, 최왼쪽으로 부터
-      // 마우스 커서의 위치를 xy축으로 단위없는 px값을 리턴한다.
-      // 이 값은 사이트 전체를 이동하는 어떤 요소를 구현할 때 많이 사용
+      // 이벤트 객체하위 pageX, pageY는 최상단, 최왼쪽으로부터
+      // 마우스 커서의 위치를 x,y축으로 단위없는 px값을 리턴한다!
+      // 이 값은 사이트 전체를 이동하는 어떤 요소를 구현할때
+      // 많이 사용함
       // console.log("pageX:",헐.pageX);
       // console.log("pageY:",헐.pageY);
 
       // 여기서는 본 박스 안에서만 그 위치를 알면 되므로
-      // -> offsetX, offsetY -> 해당 부모 요소 박스로 부터 위치를 리턴함
+      // -> offsetX, offsetY -> 해당부모요소 박스로 부터
+      // 위치를 리턴함!
       // console.log(
       //   "offsetX:",
       //   헐.offsetX
@@ -117,7 +121,7 @@ const arr1 = new Array(
       //   헐.offsetY
       // );
 
-      // 위치값 반영대상 : 김태리 이미지 -> kim
+      // 위치값 반영대상 : 김태리 이미지 -> #kim
       kim.style.left = `${헐.offsetX}px`;
       kim.style.top = `${헐.offsetY}px`;
     };
@@ -162,16 +166,16 @@ target[0].innerHTML = `
 // 배열안에 배열이 또 있으면 대괄호를 추가하여
 // 해당 순번을 써준다! 변수[순번][순번]
 
-// 김태리 기능 추가!
-// 함수 호출 : 배열 마지막 번호 == 배열개수 - 1
+// 김태리 기능추가!
+// 배열마지막 번호 == 배열개수-1
 target[0].onclick =
   arr1[arr1.length - 1];
 
-// 클릭 이벤트 강제 실행!
+// click 이벤트 강제 실행!
 // click() 메서드 호출!
 target[0].click();
 
-// 1-2. 배열 리터럴 방식의 선언과 할당 /////////////////////////////
+// 1-2. 배열 리터럴 방식의 선언과 할당 /////////////////////
 // 배열변수명 = [값1, 값2,...]
 // new 키워드 없이 바로 쓸 수 있는 객체임!
 // 이런 배열과 같은 객체를 정적객체(Static Object)라고 함!
@@ -182,18 +186,20 @@ const arr2 = [
   1919,
   function () {
     alert("대한독립만세~!");
-    // this는 누구인가? 호출한 요소 자신
-    console.log("this: ", this);
-    // this에 배경 넣기
-    this.style.background = `url(https://blog.kakaocdn.net/dn/H4k8p/btqUUqx7TLT/VAMfjsV79wqyKIfOGXn5P0/img.jpg) repeat-x 0/auto 100%`;
-    // 트랜스폼 변경: 스케일 1.5, 회전 720도
+    // this는 누구인가? 호출한 요소자신!
+    console.log("this:", this);
+    // 배경넣기
+    this.style.background = `
+    url(https://blog.kakaocdn.net/dn/H4k8p/btqUUqx7TLT/VAMfjsV79wqyKIfOGXn5P0/img.jpg) repeat-x 0/auto 100%`;
+    // 트랜스폼 변경 : 스케일 1.5, 회전 720도
     this.style.scale = "1.5";
     this.style.rotate = "720deg";
     // 트랜지션 : 2초 ease-in-out
     this.style.transition =
       "2s ease-in-out";
 
-    // 4초 후에 다시 원래 크기로 돌아가기
+    // 4초후에 다시 원래크기로 돌아가기
+    // setTimeout(함수,시간)
     setTimeout(() => {
       this.style.scale = "1";
     }, 4000);
@@ -212,8 +218,11 @@ console.log(
   Array.isArray(arr2) ? "응" : "아니"
 );
 
-// 출력대상: target의 두번째
-target[1].innerHTML = `${arr2[0]}은 ${arr2[2]}년에 일제에 항거하여 ${arr2[1]}를 들고 일어난 민중봉기를 기념하는 날이다!`;
+// 출력대상 : target의 두번째
+target[1].innerHTML = `
+    ${arr2[0]}은 ${arr2[2]}년에 일제에 항거하여
+    ${arr2[1]}를 들고 일어난 민중봉기를 기념하는 날이다!
+`;
 
 // 두번째 출력박스를 클릭하면 배열 값에 있는 함수 호출하기
 target[1].onclick = arr2[3];
@@ -221,68 +230,68 @@ target[1].onclick = arr2[3];
 // 두번째 박스에 타이틀 출력
 target[1].title =
   "클릭하시면 만세를 합니다!";
-
-// 두번째 박스에 손가락 표시
+// 두번째 박스에 손가락표시
 target[1].style.cursor = "pointer";
 
-// console.log("배열안의 함수 : ", arr2[3]);
+// console.log("배열안의 함수:",arr2[3]);
 
-// 1-3. 배열을 미리 생성하여 각각 할당하기 //////////////////
+// 1-3. 배열을 미리 생성하여 각각 할당하기 //////////
 // 배열변수명 = [] -> 배열리터럴
-// 배열변수명.length = 숫자 -> 숫자만크ㅏㅁ 배열이 생성됨
-// 배열변수명.length 는 배열 개수를 앍기/쓰기/ 모두 가능
+// 배열변수명.length = 숫자 -> 숫자만큼 배열이 생성됨
+// 배열변수명.length 는 배열개수를 읽기/쓰기 모두 가능함!
 
 const arr3 = [];
-// const 상수로 리터럴 선언, 할당 후
+// const 상수로 리터럴선언, 할당후
 // 배열값 변경은 자유롭다!
-// 그러나 배열형을 변경할 수 없다! 즉, 재할당 불가!!
-// 그래서 상수임! (코드 보안상, 안전상 이유로 많이 사용함)
+// 그러나 배열형을 변경할 수 없다! 즉, 재할당불가!!!
+// 그래서 상수임! (코드 보안상, 안전상 이유로 많이씀)
 
-console.log("arr3:", arr3);
+console.log("arr3배열:", arr3);
 
 // 배열개수 미리 셋팅하기
 arr3.length = 8;
-// 베열의 개수를 미리 셋팅해도 배열을 더 추가할 수 있음!
-// 의미는? 이미 배열 개수를 정하고 이것을 지키려는 의도임!
+// 배열의 개수를 미리 셋팅해도 배열을 더 추가할 수 있음!
+// 의미는? 미리 배열개수를 정하고 이것을 지키려는 의도임!
 
-console.log("arr3:", arr3);
+console.log("arr3배열:", arr3);
 
-// 각 배열 주소에 값을 할당
+// 각 배열주소에 값을 할당하기
 arr3[0] = "산";
 arr3[1] = "할아버지";
 arr3[2] = "구름모자";
-arr3[3] = "썻네~!";
+arr3[3] = "썼네~!";
 arr3[4] = "나비같이";
 arr3[5] = "훨훨";
 arr3[6] = "훠얼훨";
 arr3[7] = "날아서~!";
 
-// 배열 전체값 출력하기 : valueOf()
+// 배열전체값 출력하기 : valueOf()
 console.log(
-  "arr3 전체값:",
+  "arr3배열전체값:",
   arr3.toString()
 );
 console.log(
-  "arr3 전체값:",
+  "arr3배열전체값:",
   arr3.valueOf().toString()
 );
-// 현재 브라우저는 valueOf()를 사용하지 않아도 배열 값을 출력해줌
-// toString() 출력은 배열값을 콤마로 연결한 문자형으로 변환한다
+// 현재 브라우저는 valueOf()안해도 배열값을 보여준다!
+// toString() 출력은 배열값을 콤마로 연결한 문자열로 변환한다!
 
 // 변수값 사이에 구분자 넣고 문자형으로 배열값 변경하기
-// join(구분자) -> 배열의 요소를 구분자로 연결
+// join(구분자) -> 구분자 넣고 문자값 생성
 console.log(
-  "arr3 전체값:",
-  arr3.join("♥︎")
+  "arr3배열 join():",
+  arr3.join("♥")
 );
 
 // 배열값 맨뒤에 값 추가하기 메서드 : push()
-arr3.push("김창환 작사");
+arr3.push("김창환작사");
 
-// 배열값을 세번째 target박스에 출력
+// 배열값을 세번째 target박스에 출력하기
+// join()으로 사이에 별표 넣고 문자열 변환출력
 target[2].innerHTML = arr3.join("★");
 
-// -> 배열 메서드는 매우 중요! 별도로 훈련 필요
+// -> 배열 메서드는 중요하므로 별도로 훈련함!!!
 
 /********************************************
           [ 객체(Object) 란? ]
@@ -331,31 +340,30 @@ target[2].innerHTML = arr3.join("★");
   - 객체명[속성명]
   또는
   - 객체명.속성명
+
 ********************************************/
 
-// 2. 객체를 셋팅하고 출력하기 ////////////
-/* 
-  2-1. new 키워드로 Object객체 생성하여 셋팅하기
-    -> 실제 코드에서는 객체 리터럴 방식을 쓴다
-    즉, 변수 = {}
-    여기서는 new 키워드 생성 방식도 된다는 것을 체험함
-    객체는 셋팅시 반드시 중괄호{}를 사용함
-    중괄호 안에는 {속성명:갑,속성명:갑,속성명:갑,........}
-    */
+// 2. 객체를 셋팅하고 출력하기 ////////
+// 2-1. new 키워드로 Object객체 생성하여 셋팅하기
+// -> 실제 코드에서는 객체 리터럴 방식을 쓴다
+// 즉, 변수 = {}
+// 여기서는 new키워드 생성 방식도 된다는 것을 체험함!
+// 객체는 셋팅시 반드시 중괄호{}를 사용함!!!
+// 중괄호안에는 {속성명:값,속성명:값,...}
 const SSG = new Object({
   "너의 이름은?": "손석구",
   생일: "1983년 2월 7일",
   키: "178cm",
   몸무게: "80kg",
-  혈액형: "B형",
+  혈액형: "C형",
   성별: "남성",
   대표작: "나의 해방일지,범죄도시2",
   소속사: "셋별당엔터",
   비밀번호: 7777,
   팬레터: function () {
-    // this키워드 : 이벤트 호출 요소 자신!
+    // this키워드 : 이벤트호출요소 자신!
     alert("상남자 오빠! 지금뭐해?");
-    console.log("this: ", this);
+    console.log("this:", this);
 
     // 변경대상 : this.style
     let mycss = this.style;
@@ -363,50 +371,47 @@ const SSG = new Object({
     // 1. 배경변경
     mycss.background =
       "url(https://file.mk.co.kr/meet/neds/2022/05/image_readtop_2022_456627_16533579475052374.jpeg) repeat-x top/auto 100%";
-
     // 2. 글자색
     mycss.color = "#fff";
-
     // 3. 글자그림자
     mycss.textShadow = "0 0 5px #000";
-
-    // 4. 줄간격 변경
+    // 4. 줄간격변경
     mycss.lineHeight = "84px";
-
-    // 5. 박스 크게 만들기
+    // 5. 박스 확대
     mycss.scale = "1.2";
-
     // 6. 트랜지션
     mycss.transition = "1s ease-out 1s";
 
-    // 글자내용변경
+    // 7. 글자내용변경
     this.innerText = `손석구 최고 멋쨍이! 승승장구! 화이팅!!!`;
-
-    setTimeout(() => {
-      this.style.scale = "1";
-    }, 4000);
   },
-}); ///////// SSG 객체 //////////
+}); ///// SSG 객체 ///////////
 
-console.log("손석구 객체", SSG);
+console.log("석구객체:", SSG);
 
-// 박스에 출력 전 셋팅 변경
+// 박스에 출력전 셋팅변경 ///
 // 대상박스 : target[3] 네번째 박스
 
 // 줄간격 2줄이니까 조정
 target[3].style.lineHeight = "40px";
 
 // 툴팁 넣기
-// 객체호출법 2가지
+// 객체호출법 2가지 :
 // 1) 객체명.속성명
-// 2) 객체명.[문자형속성명]
+// 2) 객체명[문자형속성명]
 target[3].title = `여기를 클릭하여 ${SSG["너의 이름은?"]}팬레터를 확인하세요!`;
 
-// 손가락 모양 커서
+// 손가락모양 커서
 target[3].style.cursor = "pointer";
-
+let aa = "생일";
 // 출력하기
-target[3].innerHTML = `당신이 좋아하는 남자배우는? ${SSG["너의 이름은?"]} / 몸무게를 아세요? ${SSG.몸무게} <br> 생년월일은? ${SSG["생일"]} / 대표작은? ${SSG.대표작}`;
+target[3].innerHTML = `
+  당신이 좋아하는 남자배우는? 
+  ${SSG["너의 이름은?"]}
+  / 몸무게를 아세요? ${SSG.몸무게} <br>
+  생년월일은? ${SSG["생일"]}
+  / 대표작은? ${SSG.대표작}
+`;
 
 // 객체의 함수를 이벤트에 연결하기
 // 특히 객체의 함수를 메서드라고 부른다!
@@ -416,40 +421,40 @@ target[3].addEventListener(
 );
 
 /***************************************
-    [ new 키워드 없이 바로 객체 생성하기 ]
-    -> 객체 리터럴 (추천방식!)
+  [ new 키워드 없이 바로 객체 생성하기 ]
+  -> 객체 리터럴 (추천방식!)
 
-    - 방법: 변수 선언 후 이퀄 뒤에 바로 중괄호 사용!
-    예) let obj = {속성명:값,속성명:값,...};
+  - 방법: 변수 선언 후 이퀄 뒤에 바로 중괄호 사용!
+  예) let obj = {속성명:값,속성명:값,...};
 
-    [ 객체의 속성 셋팅시
-    문자형 또는 변수형 사용하기 ]
+  [ 객체의 속성 셋팅시
+  문자형 또는 변수형 사용하기 ]
 
-    1. 문자형 속성 - 따옴표로 싸는 방법
+  1. 문자형 속성 - 따옴표로 싸는 방법
 
-    예) let obj = {"나는나":"호호호","너는너":"하하하"}
-    -> 문자형 속성의 객체 호출시
-    객체명[문자형속성명]
-    예) obj["나는나"]
+  예) let obj = {"나는나":"호호호","너는너":"하하하"}
+  -> 문자형 속성의 객체 호출시
+  객체명[문자형속성명]
+  예) obj["나는나"]
 
-    2. 변수형 속성 - 따옴표로 안싸는 방법
-    예) let obj = {name:"김수현",tall:"186cm"};
-    -> 변수형 속성의 객체 호출시
-    객체명.속성명
-    예) obj.name
+  2. 변수형 속성 - 따옴표로 안싸는 방법
+  예) let obj = {name:"김수현",tall:"186cm"};
+  -> 변수형 속성의 객체 호출시
+  객체명.속성명
+  예) obj.name
 
-    또는
+  또는
 
-    객체명["속성명"]
-    예) obj["name"]
-    -> 반드시 변수형 속성명을 따옴표로 싸서
-    문자형으로 표시해야함!
-    obj[name] -> 에러남!
+  객체명["속성명"]
+  예) obj["name"]
+  -> 반드시 변수형 속성명을 따옴표로 싸서
+  문자형으로 표시해야함!
+  obj[name] -> 에러남!
 
-    -> 만약 문자형으로 설정된 경우에도
-    변수형으로 사용될 수 있는 문자면 변수형호출가능!
-    예) var obj = {"하하하":"나나나"}
-        obj["하하하"] 또는 obj.하하하
+  -> 만약 문자형으로 설정된 경우에도
+  변수형으로 사용될 수 있는 문자면 변수형호출가능!
+  예) var obj = {"하하하":"나나나"}
+      obj["하하하"] 또는 obj.하하하
 
 ***************************************/
 
@@ -460,15 +465,43 @@ const GU = {
   weight: "74kg",
   com: "매니지먼트숲",
   work: "도깨비,부산행",
-  msgFn: function (txt) {
+  msgFn: function (txt, ele) {
+    // txt - 메시지, ele - 호출요소
     // 1. 메시지 띄우기(호출확인!)
     alert("팬레터:" + txt);
+
     // this의 의미는?
     // 1)만약 함수를 별도로 호출하였으면
     // 객체안의 메서드이므로 객체자신임!
     // 2)만약 이벤트설정이 직접 할당되었으면
     // 호출한 요소 자신이 this임!
     console.log("this:", this);
+
+    // 2. CSS변경하기 : ele - 호출한 요소 자신
+    let mycss = ele.style;
+
+    // 2-1.배경이미지넣기
+    mycss.background =
+      "url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Gong_Yoo_%28Sep_2016%29.png/250px-Gong_Yoo_%28Sep_2016%29.png) repeat-x top/auto 100%";
+
+    //2-2. 글자색
+    mycss.color = "#fff";
+
+    //2-3. 글자그림자
+    mycss.textShadow = "0 0 5px #000";
+
+    //2-4. 줄간격
+    mycss.lineHeight = "30px";
+
+    //2-5. 높이값 변경
+    mycss.height = "100px";
+
+    //2-6. 상단패딩
+    mycss.paddingTop = "100px";
+
+    //2-7. 트랜지션
+    mycss.transition =
+      "2s ease-in-out .5s";
   },
 }; //////// GU객체 ////////////
 
@@ -587,6 +620,7 @@ target[5].style.lineHeight = "34px";
 target[5].style.cursor = "pointer";
 target[5].style.fontSize = "20px";
 
+
 // 할당형 함수는 바로 호출시 하단에서 해야함!
 showMovieInfo();
 
@@ -595,15 +629,17 @@ showMovieInfo();
 //////////////////////////////////
 // 버튼에 사용할 이름 배열만들기
 const choiceName = [
-  "전정훈",
+  "탐쌤",
   "김혜민",
   "양현석",
   "황대웅",
   "이민경",
   "이민지",
+  "전정훈",
   "윤고은",
   "김다영",
   "강수현",
+  "훈련생10",
 ];
 
 // 배열확인
@@ -686,9 +722,9 @@ for (let i = 0; i < cntBtn; i++) {
 
     switch (btnTxt) {
       // 자기자신 오브젝트
-      case "전정훈초이스":
+      case "탐쌤초이스":
         // 객체의 내용을 덮어쓰기 변경함
-        tomObj = jjhObj;
+        tomObj = myObj;
         break;
 
       case "김혜민초이스":
@@ -711,17 +747,26 @@ for (let i = 0; i < cntBtn; i++) {
         tomObj = mimObj;
         break;
 
+      case "전정훈초이스":
+        tomObj = jjhObj;
+        break;
+
       case "윤고은초이스":
         tomObj = ygeObj;
+        break;
+
+      case "김다영초이스":
+        tomObj = ssgObj;
         break;
 
       case "강수현초이스":
         tomObj = shkObj;
         break;
 
-      case "김다영초이스":
-        tomObj = kdyObj;
+      case "훈련생10초이스":
+        tomObj = ssgObj;
         break;
+
     }
 
     console.log(
