b7f61f0ee3b68e3757412fec5e4f4d8602a79565
ToffeeSta <Wjdgnsrh@gmail.com>
2025-02-06 12:36:48 +0900

    ㅇ

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/router.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/router.js
@@ -8,24 +8,9 @@ import { JoinComp } from "./components/join.js";
 import { LoginComp } from "./components/login.js";
 import { TodayAreaComp } from "./components/today_area.js";
 import { FashionAreaComp } from "./components/fashion_area.js";
+import { MainComp } from "./components/main.js";
 
 // console.log(TopComp, BannerComp, JoinComp, LoginComp);
-
-// 메인페이지 컴포넌트 : 구성할 여러개의 컴포넌트호출 ////
-const MainComp = Vue.component("main-comp",{
-    template: `
-        <main>
-            <!-- 배너영역 -->
-            <banner-comp></banner-comp>
-
-            <!-- Today’s Stories 영역 -->
-            <today-area-comp></today-area-comp>
-
-            <!-- fashion 영역 -->
-            <fashion-area-comp></fashion-area-comp>
-        </main>
-    `
-}); ///////////// MainComp ///////////////////
 
 // [ 뷰라우터 인스턴스 내보내기 ] /////
 // -> 라우터 셋팅 방식
@@ -35,14 +20,25 @@ const MainComp = Vue.component("main-comp",{
 // -> new VueRouter({routes})
 // -> routes라는 속성명은 정해진이름임!
 export default new VueRouter({
-    routes: [
-      {
-        // (1) 경로설정 : path
-        // -> router-link의 to 속성값과 같은값으로 셋팅!
-        path: "/", // 슬래쉬(/)만 쓰면 첫페이지임!
-        // (2) 연결할 컴포넌트 설정 : component
-        // -> 외부의 변수로 셋팅할 수 있고 직접 쓸 수 있음
-        component: MainComp,
-      },
+  routes: [
+    // [ 루트 첫 페이지 셋팅 ]
+    {
+      // (1) 경로설정 : path
+      // -> router-link의 to 속성값과 같은값으로 셋팅!
+      path: "/", // 슬래쉬(/)만 쓰면 첫페이지임!
+      // (2) 연결할 컴포넌트 설정 : component
+      // -> 외부의 변수로 셋팅할 수 있고 직접 쓸 수 있음
+      component: MainComp,
+    },
+    // [ 회원가입 페이지 셋팅 ]
+    {
+      path: "/join",
+      component: JoinComp,
+    },
+    // [ 로그인 페이지 셋팅 ]
+    {
+      path: "/login",
+      component: LoginComp,
+    },
   ],
-  }); ///////// VueRouter ////////////////
+}); ///////// VueRouter ////////////////
